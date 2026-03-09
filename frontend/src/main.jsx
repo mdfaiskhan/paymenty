1a655ad6f4114cb4faee00ea10577a70ed92511e
Mohammed Faisal Khan <mdfaiskhan@gmail.com>
2026-03-09 15:50:11 +0530

    Use HashRouter to prevent refresh 404 on static hosting

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 import { AuthProvider } from "./context/AuthContext";
 import { AnalyticsProvider } from "./context/AnalyticsContext";
@@ -9,12 +9,12 @@ import "./styles/components.scss";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <AuthProvider>
         <AnalyticsProvider>
           <App />
         </AnalyticsProvider>
       </AuthProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
